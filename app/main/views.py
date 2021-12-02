a51bbbc06632470b55a599be8bc3ddecbe13401c
Nevena Turkovic <nevenatu@gmail.com>
2021-12-02 17:59:29 +0100

    Home route.

--- app/main/views.py
+++ app/main/views.py
@@ -18,3 +18,8 @@ from ..decorators import permission_required
 @main.route("/")
 def index():
     return render_template("index.html")
+
+
+@main.route("/home")
+def home():
+    return render_template("home.html")
